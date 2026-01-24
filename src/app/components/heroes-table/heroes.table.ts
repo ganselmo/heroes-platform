a5102ea4e430ac400eade562bed4514837f91e2a
Gerónimo Anselmo <geronimoalejandroanselmo@gmail.com>
2026-01-24 03:14:49 -0300

    Add table pagination

--- src/app/components/heroes-table/heroes.table.ts
+++ src/app/components/heroes-table/heroes.table.ts
@@ -1,7 +1,9 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { Hero } from '../../models/hero.model';
+
+const PAGE_SIZE = 10;
 
 @Component({
   selector: 'app-heroes-table',
@@ -12,4 +14,26 @@ import { Hero } from '../../models/hero.model';
 export class HeroesTable {
   heroes = input<Hero[]>([]);
   headerRowDefinition = ['id', 'name', 'franchise', 'description', 'action'];
+
+  page = signal(0);
+  pageSize = signal(PAGE_SIZE);
+
+  pagedHeroes = computed(() => {
+    const start = this.page() * this.pageSize();
+    return this.heroes().slice(start, start + this.pageSize());
+  });
+
+  totalPages = computed(() => Math.ceil(this.heroes().length / this.pageSize()));
+
+  nextPage(): void {
+    if (this.page() < this.totalPages() - 1) {
+      this.page.update((page) => page + 1);
+    }
+  }
+
+  prevPage(): void {
+    if (this.page() > 0) {
+      this.page.update((page) => page - 1);
+    }
+  }
 }
